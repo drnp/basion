a8556475cb7d56ae3ac6b15621acc05ade5a318e
Dr.NP <np@bsgroup.org>
2016-05-13 11:26:02 +0800

    Add auto mode

--- Basion/MiddleWares/ContentType.php
+++ Basion/MiddleWares/ContentType.php
@@ -33,6 +33,7 @@ class ContentType implements MiddleWareInterface
     ];
 
     private static $supported_content_type = [
+        'text/plain' => 'raw', 
         'application/x-www-form-urlencoded' => 'php', 
         'multipart/form-data' => 'php', 
         'text/xml' => 'xml', 
@@ -74,33 +75,65 @@ class ContentType implements MiddleWareInterface
     public static function before()
     {
         $provider = \func_get_arg(0);
-
-        // HTTP accept
-        $accept = \filter_input(\INPUT_SERVER, 'HTTP_ACCEPT');
-        $acceptable = \explode(',', $accept);
-        $accept_content_types = [];
-        foreach ($acceptable as $item)
+        $config = $provider['middlewares']['config']['ContentType'];
+        if (!isset($config['output']))
         {
-            $q = 1;
-            $tmp = \explode(';q=', $item);
-            $type = $tmp[0];
-            if (isset($tmp[1]))
-            {
-                $q = \floatval($tmp[1]);
-            }
-
-            $accept_content_types[\strtolower(\trim($type))] = \floatval($q);
+            $config['output'] = 'auto';
         }
 
-        \arsort($accept_content_types, \SORT_NUMERIC);
-        foreach ($accept_content_types as $type => $q)
+        // HTTP Accept
+        switch ($config['output'])
         {
-            if (isset(self::$supported_accept[$type]))
-            {
-                self::$accept = $type;
-                self::$encode_method = self::$supported_accept[$type];
+            case 'auto' : 
+                $accept = \filter_input(\INPUT_SERVER, 'HTTP_ACCEPT');
+                $acceptable = \explode(',', $accept);
+                $accept_content_types = [];
+                foreach ($acceptable as $item)
+                {
+                    $q = 1;
+                    $tmp = \explode(';q=', $item);
+                    $type = $tmp[0];
+                    if (isset($tmp[1]))
+                    {
+                        $q = \floatval($tmp[1]);
+                    }
+
+                    $accept_content_types[\strtolower(\trim($type))] = \floatval($q);
+                }
+
+                \arsort($accept_content_types, \SORT_NUMERIC);
+                foreach ($accept_content_types as $type => $q)
+                {
+                    if (isset(self::$supported_accept[$type]))
+                    {
+                        self::$accept = $type;
+                        self::$encode_method = self::$supported_accept[$type];
+                        break;
+                    }
+                }
+
                 break;
-            }
+            case 'json' : 
+                self::$encode_method = 'json';
+                self::$accept = 'application/json';
+                break;
+            case 'xml' : 
+                self::$encode_method = 'xml';
+                self::$accept = 'application/xml';
+                break;
+            case 'yaml' : 
+                self::$encode_method = 'yaml';
+                self::$accept = 'application/x-yaml';
+                break;
+            case 'php' : 
+                self::$encode_method = 'php';
+                self::$accept = 'text/plain';
+                break;
+            case 'normal' : 
+            default : 
+                self::$encode_method = 'normal';
+                self::$accept = 'text/plain';
+                break;
         }
 
         // HTTP Content-type (POST | PUT)
@@ -119,12 +152,23 @@ class ContentType implements MiddleWareInterface
             $raw = \file_get_contents('php://input');
             switch (self::$decode_method)
             {
+                case 'raw' : 
+                    $post_data = $raw;
+                    break;
                 case 'php' : 
                     $post_data = $_POST;
                     break;
                 case 'xml' : 
                     $service = new \Sabre\Xml\Service();
-                    $post_data = $service->parse($raw);
+                    try
+                    {
+                        $post_data = $service->parse($raw);
+                    }
+                    catch (\Sabre\Xml\ParseException $e)
+                    {
+                        // Just ignore
+                        $post_data = [];
+                    }
                     break;
                 case 'json' : 
                     $post_data = \json_decode($raw, true);
@@ -136,7 +180,59 @@ class ContentType implements MiddleWareInterface
                     break;
             }
 
-            $input = \array_merge($input, $post_data);
+            if (\is_array($post_data))
+            {
+                $input = \array_merge($input, $post_data);
+            }
+            elseif (\is_string($post_data))
+            {
+                // Raw data, ignore $_GET
+                $input = $post_data;
+            }
+        }
+        elseif ('put' == $provider['http_request_method'])
+        {
+            // $_POST is empty when PUT
+            $put_data = [];
+            $raw = \file_get_contents('php://input');
+            switch (self::$decode_method)
+            {
+                case 'raw' : 
+                    $put_data = $raw;
+                    break;
+                case 'php' : 
+                    \parse_str($raw, $put_data);
+                    break;
+                case 'xml' : 
+                    $service = new \Sabre\Xml\Service();
+                    try
+                    {
+                        $put_data = $service->parse($raw);
+                    }
+                    catch (\Sabre\Xml\ParseException $e)
+                    {
+                        // Just ignore
+                        $put_data = [];
+                    }
+                    break;
+                case 'json' : 
+                    $put_data = \json_decode($raw, true);
+                    break;
+                case 'yaml' : 
+                    $put_data = \Symfony\Component\Yaml\Yaml::parse($raw);
+                    break;
+                default : 
+                    break;
+            }
+
+            if (\is_array($put_data))
+            {
+                $input = \array_merge($input, $put_data);
+            }
+            elseif (\is_string($put_data))
+            {
+                $input = $put_data;
+            }
         }
 
         $provider['input_data'] = $input;
@@ -153,7 +249,7 @@ class ContentType implements MiddleWareInterface
         {
             case 'xml' : 
                 $service = new \Sabre\Xml\Service();
-                $output = $service->write($provider['structurized_data']);
+                $output = $service->write('{basion}root', $provider['structurized_data']);
                 break;
             case 'json' : 
                 $output = \json_encode($provider['structurized_data'], \JSON_NUMERIC_CHECK | \JSON_PRETTY_PRINT);
@@ -161,10 +257,12 @@ class ContentType implements MiddleWareInterface
             case 'yaml' : 
                 $output = \Symfony\Component\Yaml\Yaml::dump($provider['structurized_data']);
                 break;
-            case 'normal' : 
             case 'php' : 
-            default : 
                 $output = \serialize($provider['structurized_data']);
+                break;
+            case 'normal' : 
+            default : 
+                $output = (\is_string($provider['structurized_data'])) ? $provider['structurized_data'] : \serialize($provider['structurized_data']);
                 break;
         }
 
